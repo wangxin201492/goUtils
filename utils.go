c8645bff97c101848b1be5baeed15af43fbaafe5
wangxin44 <wangxin44>
2016-01-06 14:29:05 +0800

    add Contain

---
+++ utils.go
@@ -0,0 +1,26 @@
+package goUtils
+
+import (
+	"errors"
+	"reflect"
+)
+
+// base from : http://www.cnblogs.com/zsbfree/archive/2013/05/23/3094993.html
+// 判断obj是否在target中，target支持的类型arrary,slice,map
+func Contain(obj interface{}, target interface{}) (bool, error) {
+	targetValue := reflect.ValueOf(target)
+	switch reflect.TypeOf(target).Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < targetValue.Len(); i++ {
+			if targetValue.Index(i).Interface() == obj {
+				return true, nil
+			}
+		}
+	case reflect.Map:
+		if targetValue.MapIndex(reflect.ValueOf(obj)).IsValid() {
+			return true, nil
+		}
+	}
+
+	return false, errors.New("not in array")
+}
